b24dde0329f6b7650ffe2985f004e3754ca29772
frogconn <frogconn@gmail.com>
2017-08-06 10:17:31 +0700

    Add seeding data

--- Startup.cs
+++ Startup.cs
@@ -61,6 +61,7 @@ namespace MvcMovie
             });
 
             DBinitialize.EnsureCreated(app.ApplicationServices);
+            SeedData.Initialize(app.ApplicationServices);
         }
     }
 }
